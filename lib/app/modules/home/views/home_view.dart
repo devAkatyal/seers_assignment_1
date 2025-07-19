075212be6cd0d4a0a0cf94a4f8e315f4f9f66d02
ayush <ayushdxb@hotmail.com>
2025-07-19 15:59:33 +0530

    Initial Refactor

---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:seers_assignment/app/modules/home/controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Hello World')));
+  }
+}
